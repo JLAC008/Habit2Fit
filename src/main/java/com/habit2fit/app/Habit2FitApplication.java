91de5189eafa4865641cdeadc010416cf07a7921
Jose Luis Alonso <joseluji223@gmail.com>
2025-04-23 21:51:13 +0200

    Primer commit

---
+++ src/main/java/com/habit2fit/app/Habit2FitApplication.java
@@ -0,0 +1,13 @@
+package com.habit2fit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Habit2FitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Habit2FitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/habit2fit/app/Habit2FitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habit2fit.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Habit2FitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
